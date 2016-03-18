1796da591afc11310b2f50d04ec9d1f061d81eec
Jude Taylor <me@jude.bio>
2016-03-18 02:19:10 -0700

    add overriding to release.nix

--- release.nix
+++ release.nix
@@ -11,9 +11,32 @@ genAttrs supportedCompilers (compiler:
     with import <nixpkgs> { inherit system; };
 
     let
-      haskellPackages = if compiler == "default"
+      haskellPackages = (if compiler == "default"
         then pkgs.haskellPackages
-        else pkgs.haskell.packages.${compiler};
+        else pkgs.haskell.packages.${compiler}).override {
+          overrides = self: super:
+            let servantHEAD = pkgs.fetchgit {
+              url = "https://github.com/haskell-servant/servant.git";
+              rev = "f5fe9a060cf2e74c9c94eef59c066e6ac913fbe4";
+              sha256 = "1v2fwjiyadia3lqhcf2hyab8rn89pxx6g8wzd8awb5jj189dsxrq";
+            }; in
+          with pkgs.haskell.lib; {
+            servant = addBuildDepends (overrideCabal super.servant (drv: {
+              version = "0.5";
+              editedCabalFile = null;
+              revision = null;
+              src = "${servantHEAD}/servant";
+            })) [ self.http-api-data self.vault ];
+            servant-docs = addBuildDepends (overrideCabal super.servant-docs (drv: {
+              version = "0.5";
+              src = "${servantHEAD}/servant-docs";
+            })) [ self.aeson-pretty self.control-monad-omega ];
+            servant-server = addBuildDepends (overrideCabal super.servant-server (drv: {
+              version = "0.5";
+              src = "${servantHEAD}/servant-server";
+            })) [ self.should-not-typecheck ];
+          };
+        };
       build = haskellPackages.callPackage ./default.nix {};
       bowerPkgs = pkgs.callPackage ./bower.nix {};
 
