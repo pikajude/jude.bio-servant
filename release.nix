c6b4ea6be48ddbe9c77b52991f03df377728c4fa
Jude Taylor <me@jude.bio>
2016-03-17 22:21:44 -0700

    matrixify release.nix

--- release.nix
+++ release.nix
@@ -1,48 +1,58 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "default", jude-web ? { outPath = ./.; } }:
+{ jude-web ? { outPath = ./.; }
 
-let
-  haskellPackages = if compiler == "default"
-    then pkgs.haskellPackages
-    else pkgs.haskell.packages.${compiler};
-  build = haskellPackages.callPackage ./default.nix {};
-  bowerPkgs = pkgs.callPackage ./bower.nix {};
+, supportedCompilers ? [ "default" "ghcHEAD" ]
+, supportedPlatforms ? [ "x86_64-linux" "i686-linux" ]
+}:
 
-  tarball = with pkgs; releaseTools.sourceTarball rec {
-    name = build.pname;
-    version = build.version;
-    src = jude-web;
-    buildInputs = [ git jq ];
+{ build = let inherit ((import <nixpkgs> {}).lib) genAttrs; in
 
-    postUnpack = ''
-      # Clean up when building from a working tree.
-      if [[ -d $sourceRoot/.git ]]; then
-        git -C $sourceRoot clean -fdx
-      fi
-    '';
+genAttrs supportedPlatforms (system:
+  genAttrs supportedCompilers (compiler:
+    with import <nixpkgs> { inherit system; };
 
-    preDist = ''
-      mkdir -p bower_components
-      for pkg in ${bowerPkgs}/packages/*/*; do
-        name="$(jq -r .name $pkg/.bower.json)"
-        cp -RL "$pkg" bower_components/"$name"
-      done
-    '';
+    let
+      haskellPackages = if compiler == "default"
+        then pkgs.haskellPackages
+        else pkgs.haskell.packages.${compiler};
+      build = haskellPackages.callPackage ./default.nix {};
+      bowerPkgs = pkgs.callPackage ./bower.nix {};
 
-    distPhase = ''
-      runHook preDist
-      tar cfj tarball.tar.bz2 * --transform 's,^,${name}/,'
-      mkdir -p $out/tarballs
-      cp *.tar.* $out/tarballs
-    '';
-  };
+      tarball = with pkgs; releaseTools.sourceTarball rec {
+        name = build.pname;
+        version = build.version;
+        src = jude-web;
+        buildInputs = [ git jq ];
 
-in {
-  job = pkgs.haskell.lib.overrideCabal build (drv: {
-    configureFlags = [ "-fopt" ];
-    doHaddock = false;
-    enableSharedExecutables = false;
-    enableSharedLibraries = false;
-    isLibrary = false;
-    src = "${tarball}/tarballs/*.tar.bz2";
-  });
-}
+        postUnpack = ''
+          # Clean up when building from a working tree.
+          if [[ -d $sourceRoot/.git ]]; then
+            git -C $sourceRoot clean -fdx
+          fi
+        '';
+
+        preDist = ''
+          mkdir -p bower_components
+          for pkg in ${bowerPkgs}/packages/*/*; do
+            name="$(jq -r .name $pkg/.bower.json)"
+            cp -RL "$pkg" bower_components/"$name"
+          done
+        '';
+
+        distPhase = ''
+          runHook preDist
+          tar cfj tarball.tar.bz2 * --transform 's,^,${name}/,'
+          mkdir -p $out/tarballs
+          cp *.tar.* $out/tarballs
+        '';
+      };
+
+    in pkgs.haskell.lib.overrideCabal build (drv: {
+      configureFlags = [ "-fopt" ];
+      doHaddock = false;
+      enableSharedExecutables = false;
+      enableSharedLibraries = false;
+      isLibrary = false;
+      src = "${tarball}/tarballs/*.tar.bz2";
+    })
+  )
+); }
